7307424d64485b4e144b670872d1c66728424627
Ahmad Habibi <ahmadhabibi7159@gmail.com>
2024-02-21 23:53:46 +0800

    update

--- main.go
+++ main.go
@@ -5,21 +5,20 @@ import (
 
 	"github.com/ahmadhabibi14/wabot/configs"
 	"github.com/ahmadhabibi14/wabot/services"
+	"github.com/ahmadhabibi14/wabot/utils"
 	_ "github.com/mattn/go-sqlite3"
 	waLog "go.mau.fi/whatsmeow/util/log"
 )
 
 func main() {
+	botName := os.Getenv("BOT_NAME")
+	utils.PrintBanner(botName)
 	configs.LoadEnv()
 	logLevel := os.Getenv("LOG_LEVEL")
-
 	dbLog := waLog.Stdout("Database", logLevel, true)
 	store := services.NewStore("sqlite3", "file:session.db?_foreign_keys=on", dbLog)
-
 	botDevice := store.GetDevice()
 	botLog := waLog.Stdout("Client", logLevel, true)
-	botName := os.Getenv("BOT_NAME")
 	bot := services.NewBot(botName, botLog, botDevice)
-
 	bot.Start()
 }
